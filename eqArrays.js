702b0c45923d10059bef2033b287964bacc30065
sachieko <kirazetsubou@gmail.com>
2022-07-26 14:40:54 -0600

    Linting eqArrays.js and removing debug code

--- eqArrays.js
+++ eqArrays.js
@@ -1,28 +1,23 @@
-// Debugging with console.assert function
-const assertEqual = (actual, expected) => {
-  console.assert(expected === actual, `🔥🔥🔥 ${actual} !== ${expected}`);
-  if (actual === expected) {
-    
-    console.log(`Assertion Passed: ✔️✔️✔️ ${actual} === ${expected}`);
-  }
-};
-
+// This cannot check nested arrays or objects.
 const eqArrays = (arrActual, arrExpected) => {
+  //If lengths are different they're not the same array.
   let bool = false;
   if (arrActual.length !== arrExpected.length) {
     return bool;
   }
+  //If any types or values are different they're different arrays.
   for (let i = 0; i < arrActual.length; i++) {
     if (arrActual[i] !== arrExpected[i]) {
       bool = false;
       return bool;
     }
-  } 
+  }
+  //If neither of these checks flagged then they are the same array.
   bool = true;
   return bool;
-}
+};
 
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
