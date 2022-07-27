b96860bf26d6ad6c6e1ed7be40210c8f258760ba
sachieko <kirazetsubou@gmail.com>
2022-07-27 13:54:34 -0600

    Refactored countOnly.js into a single function

--- countOnly2.js
+++ countOnly2.js
@@ -7,8 +7,6 @@ const assertEqual = (actual, expected) => {
   }
 };
 
-// Taking in an array allItems and object itemsToCount, for each element in the array if that key exists in the object it will count the number of times 
-// the key shows up in the array and return an object of { key: sum } pairs
 const countOnly = function(allItems, itemsToCount) {
   const resultObj = {};
   for (let item of allItems) {
