b00e7fca0e11b84c9df78dc3cc2eaa193da4939e
sachieko <kirazetsubou@gmail.com>
2022-07-27 14:04:47 -0600

    Linted countOnly.js and added countLetters.js

--- countOnly.js
+++ countOnly.js
@@ -7,7 +7,7 @@ const assertEqual = (actual, expected) => {
   }
 };
 
-// Taking in an array allItems and object itemsToCount, for each element in the array if that key exists in the object it will count the number of times 
+// Taking in an array allItems and object itemsToCount, for each element in the array if that key exists in the object it will count the number of times
 // the key shows up in the array and return an object of { key: sum } pairs
 const countOnly = function(allItems, itemsToCount) {
   const resultObj = {};
