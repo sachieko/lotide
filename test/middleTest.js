ee6ceae7e2020e5af8c7c96146eecca68b822a10
sachieko <kirazetsubou@gmail.com>
2022-08-01 14:25:10 -0600

    Refactored to use mocha chai tests

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,20 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-let test0 = [1];
-let test1 = [1, 2, 3, 4, 5];
-let test2 = [1, 2, 3, 4];
-
-assertArraysEqual(middle(test0), []);
-assertArraysEqual(middle(test1), [3]);
-assertArraysEqual(middle(test2), [2, 3]);
+describe('#middle function tests', () => {
+  it("returns [5, 6] for [4, 5, 6, 7] (2 values for even arrays)", () => {
+    assert.deepEqual(middle([4, 5, 6, 7]), [5, 6]);
+  });
+  it("returns [] for arrays with only 2 elements", () => {
+    assert.deepEqual(middle(['Poor', 'Socrates']), []);
+  });
+  it("returns [4] for [1, 2, 3, 4, 5, 6, 7] (1 value for odd arrays)", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
+  });
+  it("returns [[]] for [[], [], []] with odd elements of empty arrays", () => {
+    assert.deepEqual(middle([[], [], []]), [[]]);
+  });
+  it("returns [] for [] arrays", () => {
+    assert.deepEqual(middle([]), []);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,17 @@
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-const test = tail([3, 4, [4, 5]]);
-assertEqual(test.length, 2);
-assertArraysEqual(test, [4, [4, 5]]);
+describe('#tail function tests', () => {
+  it("returns [4, [4, 5]] for [3, 4, [4, 5]]", () => {
+    assert.deepEqual(tail([3, 4, [4, 5]]), [4, [4, 5]]);
+  });
+  it("returns ['Socrates'] for ['Poor', 'Socrates']", () => {
+    assert.deepEqual(tail(['Poor', 'Socrates']), ['Socrates']);
+  });
+  it("returns [2, 3, 4, 5, 6, 7, 8] for [1, 2, 3, 4, 5, 6, 7, 8]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4, 5, 6, 7, 8]), [2, 3, 4, 5, 6, 7, 8]);
+  });
+  it("returns [[]] for [[], []]", () => {
+    assert.deepEqual(tail([[], []]), [[]]);
+  });
+});
