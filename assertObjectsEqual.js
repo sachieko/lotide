e67d32ea4e47881200526ca376a16ae6b036fb45
sachieko <kirazetsubou@gmail.com>
2022-08-01 15:14:17 -0600

    Works with modules now

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,52 +1,6 @@
-// Object debug for poor Socrates
-// returns boolean of whether 2 arrays are equal, cannot check objects
-const eqArrays = function(arrActual, arrExpected) {
-  //If lengths are different they're not the same array, same as if either isn't an array.
-  if (arrActual.length !== arrExpected.length) {
-    return false;
-  }
-  let bool = true;
-  //If any types or values are different they're different arrays.
-  for (let i = 0; i < arrActual.length; i++) {
-    Array.isArray(arrActual[i]) ? bool = eqArrays(arrActual[i], arrExpected[i]) : bool = arrActual[i] === arrExpected[i];
-    bool === false ? false : null;
-  }
-  //If neither of these checks flagged then they are the same array.
-  return bool;
-};
-// compares Objects and returns truthy value
-const eqObjects = function(object1, object2) {
-  let bool = false;
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
-    return bool;
-  }
-  for (const key in object1) {
-    // We need some booleans so we don't compare an array or object as if they're primitives!
-    let arrayBool = Array.isArray(object2[key]);
-    let objBool = typeof object2[key] === "object" && typeof object1[key] === "object";
-    // If value for key is an array, Array.isArray() will flag it first. If typeof value returns object after this, it will be an object since it wasn't an array.
-    // If it's an array it will be compared here and if any arrays aren't equal it will return the false bool.
-    if (arrayBool) {
-      if (!eqArrays(object1[key], object2[key])) {
-        return bool;
-      }
-    }
-    // If the key isn't an array, check if it's an object.
-    if (!arrayBool && objBool) {
-      // If it's an object, we should return the false bool if the objects aren't equal. Call the object again!
-      if (!eqObjects(object1[key], object2[key])) {
-        return bool;
-      }
-    }
-    // If it's not an array or an object, we can compare primitive types and return false if they don't match.
-    if (object1[key] !== object2[key] && !arrayBool && !objBool) {
-      return bool;
-    }
-  }
-  // If it passed all flags, it should be true unless scary edge case exists.
-  bool = true;
-  return bool;
-};
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+
 
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
