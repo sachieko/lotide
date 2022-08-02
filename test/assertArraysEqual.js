8084759fadebb6a366bd20d69c989fd8a980221f
sachieko <kirazetsubou@gmail.com>
2022-08-01 20:49:31 -0600

    Fixed reference error for modules in tests

--- test/assertArraysEqual.js
+++ test/assertArraysEqual.js
@@ -1,6 +1,6 @@
 //Debugging array functions
 //Poor Socrates
-const eqArrays = require('./eqArrays');
+const eqArrays = require('../eqArrays');
 //assert whether two arrays are equal and log a message to console depending on result
 const assertArraysEqual = function(actual, expected) {
   let bool = eqArrays(actual, expected) === true;
--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,5 +1,6 @@
 //Outdated due to Mocha chai testing
-const assertArraysEqual = require('../assertArraysEqual.js');
+const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]); // => pass
 assertArraysEqual([1, 2, 3], [3, 2, 1]); // => fail
