7d05c67fe68a8f5cc0ef640ac55acfa1a00232a7
sachieko <kirazetsubou@gmail.com>
2022-08-01 21:14:16 -0600

    Moved old test code

--- test/assertEqual.js
+++
@@ -1,9 +0,0 @@
-// Debugging with console.assert function
-// Poor Socrates
-const assertEqual = function(actual, expected) {
-  console.assert(expected === actual, `🔥🔥🔥 ${actual} !== ${expected}`);
-  if (actual === expected) {
-    console.log(`Assertion Passed: ✔️✔️✔️ ${actual} === ${expected}`);
-  }
-};
-module.exports = assertEqual;
--- test/assertEqualTest.js
+++
@@ -1,8 +0,0 @@
-// Outdated due to Mocha chai testing.
-const assertEqual = require('./assertEqual');
-
-assertEqual("Lighthouse Labs", "Bootcamp"); // => Fail
-assertEqual(1, 1); // => Pass
-assertEqual(1, '1'); // => Fail
-assertEqual('String', 'String'); // => Pass
-assertEqual('Poor Socrates ', 'Poor Socrates '); // => Pass
